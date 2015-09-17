9ab76ae0e82f5bfd1bfd1f8649558ab6145c78ef
Ray Kuo <ray.kuo@sap.com>
2015-09-17 15:25:51 -0700

    First commit for dubai tech ed traffic jam prototype

---
+++ index.js
@@ -0,0 +1,35 @@
+var five = require("johnny-five"),
+    board, led, button;
+var channel = 'Parking Lot 1';
+var pubnub = require('pubnub').init({
+    publish_key: 'pub-c-ed90ad60-ccaa-487f-82b7-f731df7b8425',
+    subscribe_key: 'sub-c-2427eec6-0978-11e5-bf9c-0619f8945a4f'
+});
+
+board = new five.Board();
+board.on("ready", function() {
+    led = new five.Led(5);
+    button = new five.Switch(0);
+
+    button.on("open", function(value) {
+        led.off();
+
+        pubnub.publish({
+            channel: channel,
+            message: {
+                "status": "occupied"
+            }
+        });
+    });
+
+    button.on("close", function(value) {
+        led.on();
+
+        pubnub.publish({
+            channel: channel,
+            message: {
+                "status": "free"
+            }
+        });
+    });
+});
