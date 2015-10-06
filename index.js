227904cefed6cfc62044c8a525af0b9b8e2c0713
i818292 <ray.kuo@sap.com>
2015-10-06 10:36:17 -0700

    output to jpeg

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require("fs");
+var counter = 0;
 
 app.use(express.static('web'));
 app.get('/', function(req, res) {
@@ -16,8 +17,8 @@ io.on('connection', function(socket) {
     });
 
     socket.on('snapshot', function(msg) {
-    	var base64Data = msg.replace(/^data:image\/png;base64,/, "");
-        fs.writeFile("out.png", base64Data, 'base64', function(err) {
+    	var base64Data = msg.replace(/^data:image\/jpeg;base64,/, "");
+        fs.writeFile("out.jpg", base64Data, 'base64', function(err) {
             console.log(err);
         });
     });
--- web/app.js
+++ web/app.js
@@ -23,17 +23,90 @@ angular.module('mainApp', ["pubnub.angular.service", "webcam"])
         $scope.bbCanvasCtx = $scope.bbCanvas.getContext("2d");
         $scope.bbCanvasCtx.strokeStyle = "#FF0000";
 
-
+        // video
         $scope.patOpts = {
             x: 0,
             y: 0,
             w: 50,
             h: 50
         };
+        $scope.myChannel = {
+            videoWidth: 320,
+            videoHeight: 240,
+            video: null
+        };
 
         $interval(function() {
             $scope.makeSnapshot();
         }, 1000);
+
+        $scope.updateBoundingBox = function(bboxes) {
+            $scope.bbCanvasCtx.clearRect(0, 0, $scope.bbCanvas.width, $scope.bbCanvas.height);
+
+
+            // test
+            $scope.bbCanvasCtx.beginPath()
+            $scope.bbCanvasCtx.moveTo(0, 0);
+            $scope.bbCanvasCtx.lineTo(1200.0, 900.0);
+            $scope.bbCanvasCtx.lineWidth = 2;
+            $scope.bbCanvasCtx.stroke();
+
+
+            for (var i = 0; i < bboxes.length; i++) {
+                var bbox = bboxes[i];
+                $scope.bbCanvasCtx.beginPath()
+                $scope.bbCanvasCtx.rect(1200.0 * (bbox.x / 8.33) / 489, 900.0 * (bbox.y / 8.33) / 367, 1200.0 * (bbox.w / 8.33) / 489, 900.0 * (bbox.h / 8.33) / 367);
+                $scope.bbCanvasCtx.lineWidth = 2;
+                $scope.bbCanvasCtx.stroke();
+            }
+        }
+
+        $scope.makeSnapshot = function makeSnapshot() {
+            if (_video) {
+                var patCanvas = document.createElement('canvas');
+                patCanvas.width = _video.width;
+                patCanvas.height = _video.height;
+                var ctxPat = patCanvas.getContext('2d');
+
+                var idata = getVideoData($scope.patOpts.x, $scope.patOpts.y, $scope.patOpts.w, $scope.patOpts.h);
+                ctxPat.putImageData(idata, 0, 0);
+
+                console.log(patCanvas.toDataURL('image/jpeg'));
+
+                // send to server to dump the image
+                //socket.emit('snapshot', patCanvas.toDataURL('image/jpeg'));
+            }
+        };
+
+        var getVideoData = function getVideoData(x, y, w, h) {
+            var hiddenCanvas = document.createElement('canvas');
+            hiddenCanvas.width = _video.width;
+            hiddenCanvas.height = _video.height;
+            var ctx = hiddenCanvas.getContext('2d');
+            ctx.drawImage(_video, 0, 0, _video.width, _video.height);
+            return ctx.getImageData(x, y, w, h);
+        };
+
+        $scope.onError = function(err) {}
+
+        $scope.onStream = function(stream) {}
+
+        $scope.onSuccess = function() {
+            _video = $scope.myChannel.video;
+            $scope.$apply(function() {
+                $scope.patOpts.w = _video.width;
+                $scope.patOpts.h = _video.height;
+            });
+        }
+
+
+
+
+
+
+        //////////////////////////////////////////////////////
+
+
 
         $scope.updateRatio = function() {
             var occupied = $scope.total - $scope.free;
@@ -61,54 +134,6 @@ angular.module('mainApp', ["pubnub.angular.service", "webcam"])
             // ctx.lineWidth = 1;
             // ctx.lineTo(300, 150);
             // ctx.stroke();
-        }
-
-        $scope.updateBoundingBox = function(bboxes) {
-            $scope.bbCanvasCtx.clearRect(0, 0, $scope.bbCanvas.width, $scope.bbCanvas.height);
-            for (var i = 0; i < bboxes.length; i++) {
-                var bbox = bboxes[i];
-                $scope.bbCanvasCtx.beginPath()
-                $scope.bbCanvasCtx.rect(1200.0 * (bbox.x / 8.33) / 489, 900.0 * (bbox.y / 8.33) / 367, 1200.0 * (bbox.w / 8.33) / 489, 900.0 * (bbox.h / 8.33) / 367);
-                $scope.bbCanvasCtx.lineWidth = 2;
-                $scope.bbCanvasCtx.stroke();
-            }
-        }
-
-        $scope.makeSnapshot = function makeSnapshot() {
-            if (_video) {
-                var patCanvas = document.createElement('canvas');
-                patCanvas.width = _video.width;
-                patCanvas.height = _video.height;
-                var ctxPat = patCanvas.getContext('2d');
-
-                var idata = getVideoData($scope.patOpts.x, $scope.patOpts.y, $scope.patOpts.w, $scope.patOpts.h);
-                ctxPat.putImageData(idata, 0, 0);
-
-                console.log(patCanvas.toDataURL());
-
-                socket.emit('snapshot', patCanvas.toDataURL());
-            }
-        };
-
-        var getVideoData = function getVideoData(x, y, w, h) {
-            var hiddenCanvas = document.createElement('canvas');
-            hiddenCanvas.width = _video.width;
-            hiddenCanvas.height = _video.height;
-            var ctx = hiddenCanvas.getContext('2d');
-            ctx.drawImage(_video, 0, 0, _video.width, _video.height);
-            return ctx.getImageData(x, y, w, h);
-        };
-
-        $scope.onError = function(err) {}
-
-        $scope.onStream = function(stream) {}
-
-        $scope.onSuccess = function() {
-            _video = $scope.channel.video;
-            $scope.$apply(function() {
-                $scope.patOpts.w = _video.width;
-                $scope.patOpts.h = _video.height;
-            });
         }
 
         $scope.updateTraffic();
