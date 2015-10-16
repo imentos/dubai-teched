ee5bac1470d97886958397f4cb97954d683b88e7
Ray Kuo <ray.kuo@sap.com>
2015-10-16 15:12:55 -0700

    test

--- karthik.py
+++ karthik.py
@@ -1,5 +1,5 @@
 from socketIO_client import SocketIO, LoggingNamespace
 
-with SocketIO('localhost', 3000, LoggingNamespace) as socketIO:
-    socketIO.emit('bbox', [{'x':10, 'y':10, 'w':100, 'h':100},{'x':10, 'y':10, 'w':100, 'h':100},{'x':10, 'y':10, 'w':100, 'h':100},{'x':10, 'y':10, 'w':100, 'h':100}])
+with SocketIO('10.48.19.240', 3000, LoggingNamespace) as socketIO:
+    socketIO.emit('car', [{'x':10, 'y':10, 'w':100, 'h':100},{'x':10, 'y':10, 'w':100, 'h':100},{'x':10, 'y':10, 'w':100, 'h':100},{'x':10, 'y':10, 'w':100, 'h':100}])
     socketIO.wait(seconds=1)
